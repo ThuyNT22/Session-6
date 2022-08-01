4f1dc459d14050c4bd4e4193fd5899df25e60feb
ThuyNT22 <109574307+ThuyNT22@users.noreply.github.com>
2022-08-01 20:28:42 +0700

    Session-6

--- Tim so gan lon nhat.cpp
+++ Tim so gan lon nhat.cpp
@@ -6,6 +6,7 @@ int main(){
 
 	int ary[n];
 	int high;
+	int min;
 	int unhigh;
 	printf("Nhap %d so nguyen:\n",n);
 	for(int i=0;i<n;i++){
@@ -19,11 +20,21 @@ int main(){
 	}
 	printf("\nSo lon nhat la %d",high);
 	
-	unhigh = 0;
+	min = ary[0];
+	for(int i=1;i<n;i++){
+		if(ary[i]<min){
+			min=ary[i];
+		}
+	}
+	if(high==min){
+		printf("Cac so da nhap bang nhau");
+	}else{
+	unhigh = min;
 	for(int i=1;i<n;i++){
 		if(ary[i]>unhigh && ary[i]<high){
 			unhigh=ary[i];
 		}
 	}
 	printf("\nSo gan lon nhat la %d",unhigh);
+	}
 }
